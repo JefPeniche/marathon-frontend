bb4c9f176f36aa74de387f1cfa3fab03c79567ad
Jef Peniche <jef.peniche@gmail.com>
2022-05-02 22:21:07 -0500

    First commit :rocket-emoji

---
+++ src/views/examples/Login.js
@@ -0,0 +1,170 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+class Login extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <main ref="main">
+          <section className="section section-shaped" style={{ 'height':'100vh'  }}>
+            <div className="shape shape-style-1 bg-gradient-default">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            <Container className="pt-lg-2">
+              <Row className="justify-content-center">
+                <Col lg="5">
+                  <Card className="bg-secondary shadow border-0">
+                    <CardHeader className="bg-white pb-5">
+                      <div className="text-muted text-center mb-3">
+                        <small>Inicia sesión con</small>
+                      </div>
+                      <div className="btn-wrapper text-center">
+                        
+                        <Button
+                          className="btn-neutral btn-icon ml-1"
+                          color="default"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <span className="btn-inner--icon mr-1">
+                            <img
+                              alt="..."
+                              src={require("assets/img/icons/common/google.svg")}
+                            />
+                          </span>
+                          <span className="btn-inner--text">Google</span>
+                        </Button>
+                      </div>
+                    </CardHeader>
+                    <CardBody className="px-lg-5 py-lg-5">
+                      <div className="text-center text-muted mb-4">
+                        <small>O inicia sesión con tus credenciales</small>
+                      </div>
+                      <Form role="form">
+                        <FormGroup className="mb-3">
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-email-83" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Correo electrónico" type="email" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-lock-circle-open" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              placeholder="Contraseña"
+                              type="password"
+                              autoComplete="off"
+                            />
+                          </InputGroup>
+                        </FormGroup>
+                        <div className="custom-control custom-control-alternative custom-checkbox">
+                          <input
+                            className="custom-control-input"
+                            id=" customCheckLogin"
+                            type="checkbox"
+                          />
+                          <label
+                            className="custom-control-label"
+                            htmlFor=" customCheckLogin"
+                          >
+                            <span>Recuérdame</span>
+                          </label>
+                        </div>
+                        <div className="text-center">
+                          <Button
+                            className="my-4"
+                            color="primary"
+                            type="button"
+                          >
+                            <a
+                              className="text-white"
+                              href="/"
+                            > INICIAR SESIÓN</a>
+                          </Button>
+                        </div>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                  <Row className="mt-3">
+                    <Col xs="6">
+                      <a
+                        className="text-light"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        <small>Olvidé mi contraseña</small>
+                      </a>
+                    </Col>
+                    <Col className="text-right" xs="6">
+                      <a
+                        className="text-light"
+                        href="/register"
+                      >
+                        <small>Crear nueva cuenta</small>
+                      </a>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </main>
+      </>
+    );
+  }
+}
+
+export default Login;
